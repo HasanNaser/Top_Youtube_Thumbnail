bc66eb9399bf6a1038b6cec221dfa8cddf61c58b
HasanNaser <19hasan0@gmail.com>
2020-12-10 08:53:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Top viewed youtube videos thumbnail 
+The thumbnails of the most viewed videos on youtube  using youtube api v3
+demo -- https://hasannaser.github.io/Top_Youtube_Thumbnail/
---
+++ src/script.js
@@ -0,0 +1,75 @@
+const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/";
+
+var HttpClient = function () {
+    this.get = function (aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function () {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
+    }
+}
+
+var getCategories = function (){
+    var client = new HttpClient();
+    let apiKey = $("#apiKey").val();
+    let api_ = YOUTUBE_API + "videoCategories"+
+        "?part=snippet"+
+        "&key="+apiKey +
+        "&regionCode=TR";
+    client.get(api_, function (response) {
+
+        let result = JSON.parse(response);
+        result.items.forEach(function (item){
+
+            let element = "<option value='"+item.id+"'>"+item.snippet.title+"</option>"
+            $("#category").append(element);
+        })
+
+        getThumbnails();
+
+    });
+
+}
+
+var getThumbnails = function (){
+    let selectedCategory = $("#category").val();
+    let apiKey = $("#apiKey").val();
+
+    if(!isNaN(selectedCategory)){
+        selectedCategory=27;
+    }
+    let apiurl = YOUTUBE_API+"search" +
+        "?part=snippet" +
+        "&maxResults=50" +
+        "&type=video&fields=items(id/videoId,snippet(channelId,channelTitle,description,title,thumbnails)),nextPageToken,pageInfo,prevPageToken,regionCode" +
+        "&key="+apiKey +
+        // "&regionCode=TR"+
+        "&videoCategoryId="+selectedCategory+
+        "&publishedAfter=2020-01-01T00%3A00%3A00Z"+
+        // "&relevanceLanguage=TR"
+        "&order=viewCount";
+
+    var client = new HttpClient();
+    client.get(apiurl, function (response) {
+
+        let result = JSON.parse(response);
+        $("#result").empty();
+        result.items.forEach(function (item){
+            let element = "<a class='result-image' href='"+item.snippet.thumbnails.high.url+"' target='_blank' download><img  src='"+item.snippet.thumbnails.medium.url+"'></a>"
+            $("#result").append(element);
+        })
+
+    });
+}
+
+$("#get-result-btn").click(function () {
+    getThumbnails();
+});
+
+$("#get-categories-btn").click(function (){
+    getCategories();
+})
